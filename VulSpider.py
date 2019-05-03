4431f2ca454ee38baa793b43c21ddd58a9fc1271
c826 <ctf826@gmail.com>
2019-05-03 14:50:04 +0800

    add url re

--- VulSpider.py
+++ VulSpider.py
@@ -537,6 +537,9 @@ def send_mail_163(to_addrs, mail_Subject, mail_content, type='plain'):
 
 # qq邮箱
 def sendDingTalkMessage(title,messageUrl):
+    pattern = re.compile(r'http\S+')
+    result = pattern.findall(messageUrl)
+    messageUrl = result[0]
     url = "https://oapi.dingtalk.com/robot/send?access_token=6e6b251ed6b41f331651904bf58e30af44943f485e1803ddb3ad599d5840ce12"
     data = {}
     data['msgtype'] = 'link'
